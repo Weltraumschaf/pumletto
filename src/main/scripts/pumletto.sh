c75dc68d528f5fc5573c6d276a1281c8f7b439d5
Sven Strittmatter <sven.strittmatter@iteratec.de>
2017-11-08 19:21:08 +0100

    Make binary

---
+++ src/main/scripts/pumletto.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# JVM settings.
+jvm_min_heap_space="32m"
+jvm_max_heap_space="128m"
+jvm_options="-Xms${jvm_min_heap_space} -Xmx${jvm_max_heap_space}"
+
+program="${0}"
+
+while [ -h "${program}" ]; do
+  ls=`ls -ld "${program}"`
+  link=`expr "${ls}" : '.*-> \(.*\)$'`
+
+  if expr "${link}" : '.*/.*' > /dev/null; then
+    program="${link}"
+  else
+    program=`dirname "${program}"`/"${link}"
+  fi
+done
+
+java=java
+if test -n "$JAVA_HOME"; then
+    java="$JAVA_HOME/bin/java"
+fi
+
+exec "$java" ${jvm_options} -jar "${program}" "$@"
+exit 1
