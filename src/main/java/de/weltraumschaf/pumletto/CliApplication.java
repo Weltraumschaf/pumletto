ca000bb197d75266a792e5343850818b20a868b0
Sven Strittmatter <sven.strittmatter@iteratec.de>
2017-11-09 18:15:42 +0100

    Invoke javadoc via Doclet API

--- src/main/java/de/weltraumschaf/pumletto/CliApplication.java
+++ src/main/java/de/weltraumschaf/pumletto/CliApplication.java
@@ -1,6 +1,8 @@
 package de.weltraumschaf.pumletto;
 
+import com.sun.javadoc.*;
 import de.weltraumschaf.commons.application.InvokableAdapter;
+import com.sun.tools.javadoc.Main;
 
 /**
  * This class provides the {@link #main(java.lang.String[]) main entry point} for the command line application.
@@ -29,6 +31,25 @@ public final class CliApplication extends InvokableAdapter {
 
     @Override
     public void execute() throws Exception {
-        getIoStreams().println("Hello, world!");
+        Main.execute("",
+            Analyzer.class.getName(),
+            new String[] {"/Users/sst/src/private/pumletto/src/main/java/de/weltraumschaf/pumletto/CliApplication.java"});
+    }
+
+    // https://stackoverflow.com/questions/24727110/javadoc-source-file-parsing
+    // https://docs.oracle.com/javase/8/docs/technotes/guides/javadoc/standard-doclet.html#runningprogrammatically
+    public static class Analyzer extends Doclet {
+
+        public static boolean start(RootDoc root) {
+            for (ClassDoc classDoc : root.classes()) {
+                System.out.println("Class: " + classDoc.qualifiedName());
+
+                for (MethodDoc methodDoc : classDoc.methods()) {
+                    System.out.println("  " + methodDoc.returnType() + " " + methodDoc.name() + methodDoc.signature());
+                }
+            }
+
+            return false;
+        }
     }
 }
